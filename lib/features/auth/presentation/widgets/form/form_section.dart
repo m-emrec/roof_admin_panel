5067c6faf9a4c3542ccb038b0ef63f550f101e27
memrec45 <65484287+memrec45@users.noreply.github.com>
2025-03-16 14:40:27 +0300

    Style: FormSection width changed

--- lib/features/auth/presentation/widgets/form/form_section.dart
+++ lib/features/auth/presentation/widgets/form/form_section.dart
@@ -1,14 +1,10 @@
-import 'dart:math';
-
+import 'package:core/core.dart';
 import 'package:core/extensions/media_query_extension.dart';
 import 'package:core/resources/data_state.dart';
 import 'package:core/utils/constants/spacing_sizes.dart';
-import 'package:core/utils/logger/logger.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:logger/logger.dart';
 import 'package:roof_admin_panel/config/localization/lang/locale_keys.g.dart';
 import 'package:roof_admin_panel/features/auth/data/models/auth_model.dart';
 import 'package:roof_admin_panel/features/auth/presentation/providers/provider.dart';
@@ -39,8 +35,9 @@ class _FormSectionState extends ConsumerState<FormSection> {
     return Form(
       key: FormUtils().formKey,
       child: SizedBox(
-        width: context.dynamicWidth(0.4),
-        height: context.dynamicHeight(1),
+        width: context.dynamicWidth(0.4) > 500.0
+            ? 500.0 // This value is arbitrary.I find it suitable for the design.
+            : context.dynamicWidth(0.4),
         child: PageView(
           controller: FormUtils().pageController,
           physics: const NeverScrollableScrollPhysics(),
