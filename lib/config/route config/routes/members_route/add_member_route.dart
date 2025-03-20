69ed79bf69a1adfd9aa7c4edddcfd16394cf76f9
memrec45 <65484287+memrec45@users.noreply.github.com>
2025-03-20 22:16:26 +0300

    minor

--- lib/config/route config/routes/members_route/add_member_route.dart
+++ lib/config/route config/routes/members_route/add_member_route.dart
@@ -1,5 +1,7 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:roof_admin_panel/config/localization/lang/locale_keys.g.dart';
 import 'package:roof_admin_panel/config/route%20config/routes/base_route_class.dart';
 import 'package:roof_admin_panel/features/add%20user/presentation/pages/add_member_page.dart';
 
@@ -10,6 +12,9 @@ import 'package:roof_admin_panel/features/add%20user/presentation/pages/add_memb
 final class AddMemberRoute extends BaseRouteClass {
   @override
   String get name => "add-member";
+
+  @override
+  String get title => LocaleKeys.addMember_pageTitle.tr();
 
   @override
   Widget Function(BuildContext context, GoRouterState state) get pageBuilder =>
