a1877f04a7178dcd3e3162dda46faabda64f99cd
memrec45 <65484287+memrec45@users.noreply.github.com>
2025-04-15 12:15:52 +0300

    refactor(mentorshipWidgetService): simplify data fetching methods by consolidating logic and improving readability

--- lib/features/mentorship_widget/data/datasources/mentorship_widget_service.dart
+++ lib/features/mentorship_widget/data/datasources/mentorship_widget_service.dart
@@ -6,126 +6,63 @@ import 'package:core/utils/constants/firebase/user_doc_enum.dart';
 /// A service class for managing mentorship-related data in Firestore.
 /// It provides methods to retrieve mentor and member information based on their IDs.
 class MentorshipWidgetService extends FirebaseUtils with FirestoreUtils {
-  Future<Map<String, dynamic>?> fetchMentorForMember(String mentorId) async {
-    if (mentorId.isEmpty) {
-      return null;
-    }
-    return getDocumentRef(collection: CollectionEnum.users, docId: mentorId)
-        .get()
-        .then((value) {
-      if (value.exists) {
-        return value.data();
-      } else {
-        return null;
-      }
-    });
-  }
+  /// Fetches the mentor of the member using the provided mentor ID.
+  /// Returns a [Map] containing the mentor's data if found, otherwise returns null.
+  /// If the [mentorId] is empty, it returns null.
+  Future<Map<String, dynamic>?> fetchMentorForMember(String mentorId) async =>
+      _fetchForId(mentorId);
 
+  /// Fetches the mentat for the mentor using the provided mentat ID.
+  /// Returns a [Map] containing the mentat's data if found, otherwise returns null.
   Future<Map<String, dynamic>?> fetchMentatForMentor(
     String mentatId,
-  ) async {
-    if (mentatId.isEmpty) {
-      return null;
-    }
-    return getDocumentRef(collection: CollectionEnum.users, docId: mentatId)
-        .get()
-        .then((value) {
-      if (value.exists) {
-        return value.data();
-      } else {
-        return null;
-      }
-    });
-  }
-
-  Future<List<Map<String, dynamic>>?> fetchMembersForMentor(
-    List<String> memberIds,
-  ) async {
-    if (memberIds.isEmpty) {
-      return null;
-    }
-    return getCollectionRef(CollectionEnum.users)
-        .where(UserDocEnum.uid.name, whereIn: memberIds)
-        .get()
-        .then((value) {
-      if (value.docs.isNotEmpty) {
-        return value.docs.map((e) => e.data()).toList();
-      } else {
-        return null;
-      }
-    });
-  }
-
-  /// Retrieves the mentor information for a given member ID.
-  /// Returns a [Map] containing the mentor's data if found, otherwise returns null.
-  Future<Map<String, dynamic>?> getMentorForMember(String mentorId) async {
-    if (mentorId.isEmpty) {
-      return null;
-    }
-    return getDocumentRef(collection: CollectionEnum.users, docId: mentorId)
-        .get()
-        .then((value) {
-      if (value.exists) {
-        return value.data();
-      } else {
-        return null;
-      }
-    });
-  }
-
-  /// Retrieves the mentat information for a given mentor ID.
-  /// Returns a [Map] containing the mentat's data if found, otherwise returns null.
-  Future<Map<String, dynamic>?> getMentatForMentor(String mentatId) async {
-    if (mentatId.isEmpty) {
-      return null;
-    }
-    return getDocumentRef(collection: CollectionEnum.users, docId: mentatId)
-        .get()
-        .then((value) {
-      if (value.exists) {
-        return value.data();
-      } else {
-        return null;
-      }
-    });
-  }
-
-  /// Retrieves a list of mentors for a given list of mentor IDs.
-  /// Returns a [List] of [Map]s containing the mentors' data if found, otherwise returns null.
-  Future<List<Map<String, dynamic>>?> getMentorsForMentat(
-    List<String> mentorIds,
-  ) async {
-    if (mentorIds.isEmpty) {
-      return null;
-    }
-    return getCollectionRef(CollectionEnum.users)
-        .where(UserDocEnum.uid.name, whereIn: mentorIds)
-        .get()
-        .then((value) {
-      if (value.docs.isNotEmpty) {
-        return value.docs.map((e) => e.data()).toList();
-      } else {
-        return [];
-      }
-    });
-  }
+  ) async =>
+      _fetchForId(mentatId);
 
   /// Retrieves a list of members for a given list of member IDs.
   /// Returns a [List] of [Map]s containing the members' data if found, otherwise returns null.
-  Future<List<Map<String, dynamic>>?> getMembersForMentor(
+  Future<List<Map<String, dynamic>>?> fetchMembersForMentor(
     List<String> memberIds,
+  ) async =>
+      _fetchForListOfIds(memberIds);
+
+  /// Retrieves a list of mentors for a given list of mentor IDs.
+  /// Returns a [List] of [Map]s containing the mentors' data if found, otherwise returns null.
+  Future<List<Map<String, dynamic>>?> fetchMentorsForMentat(
+    List<String> mentorIds,
+  ) async =>
+      _fetchForListOfIds(mentorIds);
+
+  Future<List<Map<String, dynamic>>?> _fetchForListOfIds(
+    List<String> ids,
   ) async {
-    if (memberIds.isEmpty) {
+    if (ids.isEmpty) {
       return null;
     }
+
     return getCollectionRef(CollectionEnum.users)
-        .where(UserDocEnum.uid.name, whereIn: memberIds)
+        .where(UserDocEnum.uid.name, whereIn: ids)
         .get()
         .then((value) {
       if (value.docs.isNotEmpty) {
         return value.docs.map((e) => e.data()).toList();
       } else {
         return [];
+      }
+    });
+  }
+
+  Future<Map<String, dynamic>?> _fetchForId(String id) async {
+    if (id.isEmpty) {
+      return null;
+    }
+    return getDocumentRef(collection: CollectionEnum.users, docId: id)
+        .get()
+        .then((value) {
+      if (value.exists) {
+        return value.data();
+      } else {
+        return null;
       }
     });
   }
